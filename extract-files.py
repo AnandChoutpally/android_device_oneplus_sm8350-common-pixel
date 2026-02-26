3ac283f4ca830092d3c64b48b8f3dd57ceb124be
Anand Choutpally <91369446+AnandChoutpally@users.noreply.github.com>
2026-02-26 15:18:19 +0530

    Update extract-files.py

--- extract-files.py
+++ extract-files.py
@@ -20,6 +20,7 @@ from extract_utils.main import (
 namespace_imports = [
     'device/oneplus/sm8350-common',
     'hardware/oplus',
+    'hardware/pixelworks/interfaces',
     'hardware/qcom-caf/sm8350',
     'hardware/qcom-caf/wlan',
     'vendor/qcom/opensource/commonsys-intf/display',
@@ -61,6 +62,11 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('"max_retry_alloc_output_timeout": 1000,', '"max_retry_alloc_output_timeout": 0,'),
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
         .regex_replace('IGNORED_IRQ=27,23,38$', 'IGNORED_IRQ=27,23,38,115,332'),
+    (
+        'vendor/lib64/libdpps.so',
+        'vendor/lib64/libsnapdragoncolor-manager.so',
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
